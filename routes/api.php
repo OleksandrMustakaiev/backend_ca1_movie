d28d99a99927eb5116ff1c0e1167caef1aebebb2
OleksandrMustakaiev <N00202022@iadt.ie>
2022-11-17 09:38:23 +0000

    added authentication // can register and login

--- routes/api.php
+++ routes/api.php
@@ -1,11 +1,11 @@
 <?php
 
-use App\Http\Controllers\MovieController;
-use App\Http\Controllers\ProductionCompanyController;
-use App\Http\Controllers\YearController;
-use App\Models\ProductionCompany;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\AuthController;
+use App\Http\Controllers\YearController;
+use App\Http\Controllers\MovieController;
+use App\Http\Controllers\ProductionCompanyController;
 
 /*
 |--------------------------------------------------------------------------
@@ -22,7 +22,22 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-Route::apiResource('/movies', MovieController::class); // create all routes for the MovieController 
+Route::post('/auth/register', [AuthController::class, 'register']);
+Route::post('/auth/login', [AuthController::class, 'login']);
+
+// every route within these curly brackets require authentication token
+Route::middleware('auth:sanctum')->group(function (){
+    Route::post('/auth/logout',[AuthController::class, 'logout']);
+    Route::get('/auth/user',[AuthController::class, 'user']);
+
+    // You need to be logged in for all movies functionality except get all and get by id
+    Route::apiResource('/movies', MovieController::class)->except((['index', 'show']));
+});
+
+
+// Books - Define the get all and get by ID routes outside the authentication group
+Route::get('/movies', [MovieController::class, 'index']);
+Route::get('/movies/{movie}', [MovieController::class, 'show']);
 
 // this line create all routes for ProductionCompanyController
 Route::apiResource('/production_company', ProductionCompanyController::class);
